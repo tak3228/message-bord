a05a71410422440f2b34d1372cc848b441bb5283
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-01 22:31:16 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629060800) do
+ActiveRecord::Schema.define(version: 20160701111552) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
